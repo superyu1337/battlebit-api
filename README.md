b147d38d1409906cb7375547c96fcdb774451094
Janek <development@superyu.xyz>
2023-08-08 15:43:19 +0200

    Update
    - Added license
    - Added blank readme
    - removed main.rs
    - changes to cargo.toml for publishing

---
+++ README.md
@@ -0,0 +1,1 @@
+# Battlebit-api
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let api = battlebit_api::BBApi::default();
-    let server_list = api.server_list().unwrap();
-
-    println!("{server_list:#?}")
-}
